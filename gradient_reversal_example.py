3eb7de3875cb1724cd57eee1891f01d4651b40b4
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2020-05-26 16:35:43 +0200

    Implemented structure
    Add class_classifier, domain_classifier and features_extractor

--- gradient_reversal_example.py
+++ gradient_reversal_example.py
@@ -25,11 +25,44 @@ class ReverseLayerF(Function):
 
 
 class RandomNetworkWithReverseGrad(nn.Module):
-    def __init__(self, **kwargs):
+    def __init__(self, num_classes, **kwargs):
         super(RandomNetworkWithReverseGrad, self).__init__()
-        self.features = nn.Sequential(...)
-        self.classifier = nn.Sequential(...)
-        self.dann_classifier = nn.Sequential(...)
+        #features extractor:
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+
+        #class classifier
+        self.class_classifier = nn.Sequential(
+            nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, num_classes),
+        )
+
+        #domain classifier
+        self.domain_classifier = nn.Sequential(
+            nn.Linear(50 * 4 * 4, 100), nn.BatchNorm1d(100),
+            nn.ReLU(True),
+            nn.Linear(100, 2),
+            nn.LogSoftmax(dim=1),
+        )
 
     def forward(self, x, alpha=None):
         features = self.features
@@ -39,10 +72,10 @@ class RandomNetworkWithReverseGrad(nn.Module):
         if alpha is not None:
             # gradient reversal layer (backward gradients will be reversed)
             reverse_feature = ReverseLayerF.apply(features, alpha)
-            discriminator_output = ...
+            discriminator_output = self.domain_classifier(features)
             return discriminator_output
         # If we don't pass alpha, we assume we are training with supervision
         else:
             # do something else
-            class_outputs = ...
+            class_outputs = self.class_classifier(features)
             return class_outputs
