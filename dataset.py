c03de44be7c7d6a505c107470f1548459fcfdf8d
Lucia Innocenti <42211654+luciainnocenti@users.noreply.github.com>
2020-05-30 08:42:56 +0200

    meanAndStd Definition

--- dataset.py
+++ dataset.py
@@ -6,6 +6,8 @@ import os
 import os.path
 import sys
 from torchvision import transforms
+from torchvision import models
+from torch.utils.data import DataLoader
 
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
@@ -42,3 +44,20 @@ class PACS_Dataset():
     #return t(image), label
     
     return image, label
+
+  def meanAndStd(self, loader):  
+    cnt = 0
+    fst_moment = torch.empty(3)
+    snd_moment = torch.empty(3)
+
+    for images, _ in loader:
+        b, c, h, w = images.shape
+        nb_pixels = b * h * w
+        sum_ = torch.sum(images, dim=[0, 2, 3])
+        sum_of_square = torch.sum(images ** 2, dim=[0, 2, 3])
+        fst_moment = (cnt * fst_moment + sum_) / (cnt + nb_pixels)
+        snd_moment = (cnt * snd_moment + sum_of_square) / (cnt + nb_pixels)
+
+        cnt += nb_pixels
+
+    return fst_moment, torch.sqrt(snd_moment - fst_moment ** 2)
